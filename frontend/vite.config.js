ca9e1b46927ee3f923363c5d3018fc39130fd94d
rispar0529 <rishabhparihar511@gmail.com>
2025-10-22 19:10:30 +0530

    Added Dockerfiles and docker-compose.yml
    Signed-off-by: rispar0529 <rishabhparihar511@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Ecommerce-Website",
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
